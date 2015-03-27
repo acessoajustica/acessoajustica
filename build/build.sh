f915a50bc9a547a79bc2593320a6b899f75d9a1e
phrb <pedro.bruel@gmail.com>
2015-03-27 18:43:46 -0300

    First working-version of the build script.

--- build/build.sh
+++ build/build.sh
@@ -5,23 +5,29 @@
 set -e
 
 DOCKER='docker'
-DOCKER_COMPOSE='docker_compose'
+DOCKER_COMPOSE='docker-compose'
 RUBY_RACER="gem 'therubyracer', platforms: :ruby"
 COMMENT_STYLE="# "
 GEMS='Gemfile'
+DATABASE='database.yml'
+CONFIG='config'
+REMOVE='app bin config config.ru db Gemfile.lock .gitignore lib log public Rakefile README.rdoc test tmp vendor'
 
 command -v ${DOCKER}>/dev/null 2>&1 || { echo >&2 "I require '${DOCKER}', but it's not installed.  Aborting."; exit 1; }
 command -v ${DOCKER_COMPOSE}>/dev/null 2>&1 || { echo >&2 "I require '${DOCKER_COMPOSE}', but it's not installed.  Aborting."; exit 1; }
 
-docker-compose web rails new . --force --database=postgresql --skip-bundle
+cp ${GEMS} build_${GEMS}
+
+sudo docker-compose run web rails new . --force --database=postgresql --skip-bundle
 
 # Uncomment racer line
-sed -i "/${RUBY_RACER}/s/^${COMMENT_STYLE}//g" file
+sed -i "/${RUBY_RACER}/s/^${COMMENT_STYLE}//g" ${GEMS}
 
-docker-compose build
+sudo docker-compose build
 
 sudo chown -R ${USER} .
-rm config/database.yml
-mv database.yml config/
+rm ${CONFIG}/${DATABASE}
+cp ${DATABASE} ${CONFIG}/
 
-docker-compose up
+yes | rm -r ${REMOVE}
+mv build_${GEMS} ${GEMS}
