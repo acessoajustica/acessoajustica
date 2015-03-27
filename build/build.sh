1466791bc14d1332a9aaa13c12321a196eaa88c6
Pedro Bruel <pedro.bruel@gmail.com>
2015-03-27 17:36:13 -0300

    Finished build script.

--- build/build.sh
+++ build/build.sh
@@ -7,14 +7,21 @@ set -e
 DOCKER='docker'
 DOCKER_COMPOSE='diocker_compose'
 RUBY_RACER="gem 'therubyracer', platforms: :ruby"
+COMMENT_STYLE="# "
 GEMS='Gemfile'
 
 command -v ${DOCKER}>/dev/null 2>&1 || { echo >&2 "I require '${DOCKER}', but it's not installed.  Aborting."; exit 1; }
 command -v ${DOCKER_COMPOSE}>/dev/null 2>&1 || { echo >&2 "I require '${DOCKER_COMPOSE}', but it's not installed.  Aborting."; exit 1; }
 
 docker-compose web rails new . --force --database=postgresql --skip-bundle
+
 # Uncomment racer line
+sed -i "/${RUBY_RACER}/s/^${COMMENT_STYLE}//g" file
+
 docker-compose build
-# Replace database.yml
+
+sudo chown -R ${USER} .
+rm config/database.yml
+mv database.yml config/
+
 docker-compose up
-# Create db in another terminal
