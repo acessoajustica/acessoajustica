fcb4e1ec392897d8377ba0913606a2d3d43cc6ff
phrb <pedro.bruel@gmail.com>
2015-04-09 19:44:28 -0300

    New build script.

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,10 +5,10 @@
 # Starts and Restarts the app;
 # (requires docker, docker-compose and sudo privileges)
 #
-set -e
 
 DOCKER='docker'
 DOCKER_COMPOSE='docker-compose'
+GEMS='Gemfile'
 
 function check_deps {
     # Check dependencies.
@@ -16,43 +16,48 @@ function check_deps {
     command -v ${DOCKER_COMPOSE}>/dev/null 2>&1 || { echo >&2 "I require '${DOCKER_COMPOSE}', but it's not installed.  Aborting."; exit 1; }
     return 0
 }
-function build_update {
-    sudo docker-compose run web bundle update   # Updating gems.
-    sudo docker-compose run web rake spec       # Runs tests.
-}
-function build_all {
-    PROJECT_DIR=${PWD##*/}                      # Gets current dir name.
+function build_clean {
     sudo docker stop $(sudo docker ps -a -q)    # Stops old containers.
     sudo docker rm $(sudo docker ps -a -q)      # Remove old containers.
-    rm ${GEMS}.lock                             # Purges the lock.
-    sudo docker rmi ${PROJECT_DIR}_web:latest   # Removes old project image.
-
-    sudo docker-compose build                   # Builds a new image.
-    sudo docker-compose run web rake db:create  # Creates test database.
-    sudo chown -R ${USER} .                     # Owns created files.
-    sudo docker-compose run web rake db:migrate # Runs migrations.
-    sudo docker-compose run web rake spec       # Runs tests.
 }
 function app_restart {
     sudo docker stop $(sudo docker ps -a -q)    # Stops old containers.
     sudo docker-compose up                      # Starts the app.
 }
-function app_run {
-    sudo docker-compose up                      # Starts the app.
-}
 function app_migrate {
+    sudo docker-compose run web rake db:create  # Creates all databases.
     sudo docker-compose run web rake db:migrate # Runs migrations.
 }
 function app_test {
-    sudo docker-compose run web rake db:create  # Creates test database.
+    app_migrate
+    sudo docker-compose run web rake db:create  # Creates all databases.
     sudo docker-compose run web rake spec       # Runs tests.
 }
+function app_run {
+    app_migrate
+    sudo docker-compose up                      # Starts the app.
+}
+function build_update {
+    sudo docker-compose run web bundle update   # Updating gems.
+    app_test
+}
+function build_all {
+    PROJECT_DIR=${PWD##*/}                      # Gets current dir name.
+    build_clean
+    rm ${GEMS}.lock                             # Purges the lock.
+    sudo docker rmi ${PROJECT_DIR}_web:latest   # Removes old project image.
+
+    sudo docker-compose build                   # Builds a new image.
+    app_test
+}
 if check_deps; then
-    [[ $1 == all ]] && build_all ||
-    [[ $1 == update ]] && build_update ||
-    [[ $1 == restart ]] && app_restart ||
-    [[ $1 == run ]] && app_run ||
-    [[ $1 == migrate ]] && app_migrate ||
-    [[ $1 == test ]] && app_test ||
-    { echo "No arguments, exiting."; exit 0; }
+    if [[ $1 == all ]]; then build_all;
+    elif [[ $1 == update ]]; then build_update;
+    elif [[ $1 == clean ]]; then build_clean;
+    elif [[ $1 == restart ]]; then app_restart;
+    elif [[ $1 == run ]]; then app_run;
+    elif [[ $1 == migrate ]]; then app_migrate;
+    elif [[ $1 == test ]]; then app_test;
+    else { echo "No arguments, exiting."; exit 0; }
+    fi
 fi
