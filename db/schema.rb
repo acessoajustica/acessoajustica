dc7f3b71104fb91d3b68a5b1343f1291a727ea7b
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-04-22 17:58:25 -0300

    Add tables for multi-valued attributes of 'Cliente' entity (@BarbaraTieko, @Dhinihan, @renatocf)
    Used 'rails g model' to create new models without controllers/views

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411233426) do
+ActiveRecord::Schema.define(version: 20150422201748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "estado_civils", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "membros", force: :cascade do |t|
     t.date     "ano_faculdade"
