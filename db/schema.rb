55c70facaa8432accd79bcb03cb339fe3c47e9c9
Pedro Bruel <pedro.bruel@gmail.com>
2015-05-13 14:58:00 -0300

    Fix mocha install, using mocks to test calouro. (@phrb, @ghmzanon)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423200659) do
+ActiveRecord::Schema.define(version: 20150429170243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,12 +19,6 @@ ActiveRecord::Schema.define(version: 20150423200659) do
   create_table "calouros", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "estagiarios", force: :cascade do |t|
-    t.string   "especialidade"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
   end
 
   create_table "clientes", force: :cascade do |t|
@@ -48,6 +42,12 @@ ActiveRecord::Schema.define(version: 20150423200659) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "estagiarios", force: :cascade do |t|
+    t.string   "especialidade"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "membros", force: :cascade do |t|
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,7 +34,6 @@ RSpec.configure do |config|
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :mocha
-  end
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
