a2078ccdaa20b6492e545adde09dac4a1d97514f
Vinícius Nascimento Silva <vinicius.nascimento.silva@usp.br>
2015-05-20 12:21:23 -0300

    Remove unnecessary seed dependecy of the tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,10 +25,6 @@ Coveralls.wear!
 
 RSpec.configure do |config|
 
-  config.before(:suite) do
-    require "#{Rails.root}/db/seeds.rb"
-  end
-  
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
