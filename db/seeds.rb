8b9dd87283c1bebce8c4fb9bdd55188a07ff1b28
BarbaraTieko <barbara_tieko@hotmail.com>
2015-05-24 16:36:01 -0300

    Add accordion style to the list of casos (@BarbaraTieko, @vitorsamora)

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,6 +29,28 @@ ProfissaoType.create!(:description => "Primário")
 ProfissaoType.create!(:description => "Secundário")
 ProfissaoType.create!(:description => "Terciário")
 
+#Creating cliente
+cliente = Cliente.create!( :nome => "João da Silva",
+			    :cpf  =>"123.456.789-00",
+			    :nome_da_mae => "Maria d,a Penha",
+			    :rg  => "12.345.678-9",
+			    :cor  => "Negro",
+			    :identidade_de_genero => "Homem",
+			    :familia_renda  => 1500.00,
+			    :filhos_quantidade  => 6,
+			    :profissao_nome  =>"Assistente Administrativo",
+			    :familia_quantidade => 9,
+			    :contribuintes_quantidade => 4)
+
+#Creating caso
+caso1 = Caso.create!(:status => true, :cliente => cliente)
+caso2 =Caso.create!(:status => false, :cliente => cliente)
+
+#Creating relato
+relato = Relato.create!(:description => "Relato teste para teste que será testado nesse teste.", 
+				:caso => caso1)
+
 # assign the admin role to the admin user.  (This bit of rails
 # magic creates a user_role record in the database.)
 admin_user.roles << admin_role
+
