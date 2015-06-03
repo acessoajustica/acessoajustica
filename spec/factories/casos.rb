89e4332e8baf2b509a7e51e3b1cde5c8f9faf036
Vinícius Nascimento Silva <vinicius.nascimento.silva@usp.br>
2015-06-03 15:26:05 -0300

    Add unity test for caso (@mmamorim, @Dhinihan)

--- spec/factories/casos.rb
+++ spec/factories/casos.rb
@@ -5,17 +5,22 @@ FactoryGirl.define do
     cliente Cliente.new()
     # association :cliente, factory: :cliente, strategy: :build
     relatos [Relato.new({:description => 'Uma descrição'})]
-
-    trait :old do
-      created_at Date.new(1970, 1, 1)
-    end
-
-    trait :rejected do
-      status false
-    end
-
-    trait :invalid do
-      cliente nil
-    end
   end
+
+  trait :old do
+    created_at Date.new(1970, 1, 1)
+  end
+
+  trait :rejected do
+    status false
+  end
+
+  trait :invalid do
+    cliente nil
+  end
+
+  factory :caso_type do
+    description "Criminal"
+  end
+  
 end
--- spec/models/caso_spec.rb
+++ spec/models/caso_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Caso, type: :model do
+ 
   describe "get all of user" do
-    it "get all of cases of the user" do
+    it "gets all of cases of the user" do
       caso = FactoryGirl.build(:caso)
       membro = FactoryGirl.build(:membro)
 
@@ -11,5 +12,21 @@ RSpec.describe Caso, type: :model do
 
       expect(Caso.all_of_user(FactoryGirl.build(:user))).to eq([caso])
     end
+  end
+
+  describe "get description of its type" do
+    it "gets description if it exists" do
+      caso = FactoryGirl.build(:caso)
+      caso_type = FactoryGirl.build(:caso_type)
+      caso.stubs(:caso_type).returns(caso_type)
+
+      expect(caso.type_description).to eq("Criminal")
+    end
+
+    it "does not get description if it does not exist" do
+      caso = FactoryGirl.build(:caso)
+      expect(caso.type_description).to eq(nil)
+    end
+  
   end
 end
