a59f32388fb2a6adbb63292f320e39d9c841b83e
Pedro Bruel <pedro.bruel@gmail.com>
2015-06-19 15:50:16 -0300

    Fix "casos" tests (@vitorsamora, @phrb)

--- app/controllers/casos_controller.rb
+++ app/controllers/casos_controller.rb
@@ -1,5 +1,5 @@
 class CasosController < ApplicationController
-  authorize_resource
+  load_and_authorize_resource
   before_action :set_caso, only: [:show, :edit, :update, :destroy]
 
   # GET /casos
