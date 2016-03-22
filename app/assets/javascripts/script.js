c81a040be0d0f9e3ff77b96a2ab2cea6942b1ad5
BarbaraTieko <barbara_tieko@hotmail.com>
2016-03-22 19:31:55 -0300

    Hide and show 'qtd_filhos'

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,4 +3,26 @@ $(function(){
 	$(".rg").mask("99.999.999-99",{placeholder:" "});
 	$(".telefone").mask("(99) 99999-9999",{placeholder:" "});
 	$(".cep").mask("99999-999",{placeholder:" "});
+  $('.moeda').mask("###.###.##0,00", {reverse: true});
+});
+$(document).ready(function() {
+
+	$("#qtd_filhos").hide();
+
+	$("#cliente_tem_filhos_true").change(function() {
+		if($(this).is(":checked")) {
+      $("#qtd_filhos").show();
+    }
+		else{
+			$("#qtd_filhos").hide();
+		}
+	});
+	$("#cliente_tem_filhos_false").change(function() {
+		if($(this).is(":checked")) {
+      $("#qtd_filhos").hide();
+    }
+		else{
+			$("#qtd_filhos").show();
+		}
+	});
 });
