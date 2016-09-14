3a78dd2fba9b0994cfacfc4f7ee4e53415cbc4ef
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2016-09-14 12:51:33 -0300

    Start implementing test for sending email (@renatocf, @rCamposCruz)

--- spec/mailers/caso_mailer_spec.rb
+++ spec/mailers/caso_mailer_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
 RSpec.describe CasoMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+#   let(:user) { mock_model User, name: 'Rafael', email: 'rafael@email.com' }
+#   let(:mail) { described_class.feed_de_casos(user).deliver_now }
+#
+#   it 'renders the subject' do
+#     expect(mail.subject).to eq('Intimações do dia ' \
+#                                + Time.now.strftime("%d/%m/%Y"))
+#   end
+#
+#   it 'renders the receiver email' do
+#     expect(mail.to).to eq([user.email])
+#   end
+#
+#   it 'renders the sender email' do
+#     expect(mail.from).to eq(['noreplyparajas@gmail.com'])
+#   end
 end
