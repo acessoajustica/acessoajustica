a4d8722e154e57dc349c881fb08e568f196e1f85
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2016-09-14 12:49:36 -0300

    Start implementing feed of 'Casos' for 'Estagiario' (@renatocf, @rCamposCruz)

--- app/mailers/caso_mailer.rb
+++ app/mailers/caso_mailer.rb
@@ -1,2 +1,10 @@
 class CasoMailer < ApplicationMailer
+  default from: 'noreplyparajas@gmail.com'
+
+  def feed_de_casos(user)
+    @user = user
+
+    mail(to: @user.mail, subject: 'Intimações do dia ' \
+         + Time.now.strftime("%d/%m/%Y"))
+  end
 end
