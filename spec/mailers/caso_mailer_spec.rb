6b66cd3a42af2e3a985f26852ef53af9401bfcb7
Arthur Coser Marinho <arthur.marinho@gwd-web.com.br>
2016-09-16 15:01:38 -0300

    Define intermediate object passed to mailer (@arthurcoser, @ericghlee, @renatocf)

--- spec/mailers/caso_mailer_spec.rb
+++ spec/mailers/caso_mailer_spec.rb
@@ -3,8 +3,45 @@ require "rails_helper"
 RSpec.describe CasoMailer, type: :mailer do
 
    let(:user) { FactoryGirl.create(:user) }
-   let(:content) { "SEUS CASOS" }
-   let(:mail) { described_class.feed_de_casos(user, content).deliver_now }
+   let(:content) { "Fulano" }
+   let(:mail) { described_class.feed_de_casos(user, atualizacao).deliver_now }
+   let(:atualizacao) {
+    {
+        date: Time.new(2016,9,16).strftime("%d/%m/%Y"),
+        casos: [
+            {
+                id: 42,
+                nome_cliente: 'Fulano',
+                processos_update: [
+                    {
+                        processo_id: 7,
+                        diario_oficial: 'STF',
+                        secao: 'III - Avisos',
+                        atualizacao: 'HueHueHue'
+                    },
+                    {
+                        processo_id: 13,
+                        diario_oficial: 'DOM - SP',
+                        secao: 'II - Capital',
+                        atualizacao: 'Kkkkkkkkk'
+                    }
+                ]
+            },
+            {
+                id: 1789,
+                nome_cliente: 'Gubi',
+                processos_update: [
+                    {
+                        processo_id: 713,
+                        diario_oficial: 'DOM - SP',
+                        secao: 'III - Avisos',
+                        atualizacao: 'HueHueHueBRBRBRBR'
+                    }
+                ]
+            }
+        ]
+    }
+   }
 
    it 'renders the subject' do
      expect(mail.subject).to eq('Intimações do dia ' \
