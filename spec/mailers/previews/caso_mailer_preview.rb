67373954671b9ba7daac0b1756af8a8c4bb6dd53
Arthur Coser Marinho <arthur.marinho@gwd-web.com.br>
2016-09-16 16:59:30 -0300

    Preview for feed_de_casos mail (@arthurcoser, @rCamposCruz)

--- spec/mailers/previews/caso_mailer_preview.rb
+++ spec/mailers/previews/caso_mailer_preview.rb
@@ -1,4 +1,44 @@
 # Preview all emails at http://localhost:3000/rails/mailers/caso_mailer
 class CasoMailerPreview < ActionMailer::Preview
-
+  def teste
+  	@user = User.new
+  	@user.email = "teste@teste.com"
+  	@atualizacao = 
+    {
+        date: Time.new(2016,9,16).strftime("%d/%m/%Y"),
+        casos: [
+            {
+                id: 42,
+                nome_cliente: 'Fulano',
+                processos_update: [
+                    {
+                        processo_id: 7,
+                        diario_oficial: 'STF',
+                        secao: 'III - Avisos',
+                        atualizacao: 'HueHueHue'
+                    },
+                    {
+                        processo_id: 13,
+                        diario_oficial: 'DOM - SP',
+                        secao: 'II - Capital',
+                        atualizacao: 'Kkkkkkkkk'
+                    }
+                ]
+            },
+            {
+                id: 1789,
+                nome_cliente: 'Gubi',
+                processos_update: [
+                    {
+                        processo_id: 713,
+                        diario_oficial: 'DOM - SP',
+                        secao: 'III - Avisos',
+                        atualizacao: 'HueHueHueBRBRBRBR'
+                    }
+                ]
+            }
+        ]
+    }
+    CasoMailer.feed_de_casos(@user, @atualizacao)
+  end
 end
