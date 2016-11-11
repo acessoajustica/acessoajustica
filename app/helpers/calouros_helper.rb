f4ea9e3744c3cc1990a8eaffed6e0e6c8322dd2e
Arthur Coser Marinho <arthur.marinho@gwd-web.com.br>
2016-11-11 17:49:53 -0200

    Fix identation of new code

--- app/helpers/calouros_helper.rb
+++ app/helpers/calouros_helper.rb
@@ -1,8 +1,8 @@
 module CalourosHelper
 	def calouros_ordenados(calouros)
 		calouros
-				.sort_by {|calouro| calouro.membro.nome }
-                .sort_by {|calouro| calouro.membro.user.active? }
-                .sort_by {|calouro| calouro.membro.user.last_sign_in_at}
+        		.sort_by {|calouro| calouro.membro.nome }
+            .sort_by {|calouro| calouro.membro.user.active? }
+            .sort_by {|calouro| calouro.membro.user.last_sign_in_at}
 	end
 end
--- spec/helpers/calouros_helper_spec.rb
+++ spec/helpers/calouros_helper_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 describe CalourosHelper do
-    it "sorts 'calouros'" do
-      user = FactoryGirl.build(:user)
+  it "sorts 'calouros'" do
+    user = FactoryGirl.build(:user)
 
-      calouro1 = FactoryGirl.build(:calouro)
-      calouro1.membro.user = user
+    calouro1 = FactoryGirl.build(:calouro)
+    calouro1.membro.user = user
 
-      calouro2 = FactoryGirl.build(:calouro, :new_calouro)
-      calouro2.membro.user = user
+    calouro2 = FactoryGirl.build(:calouro, :new_calouro)
+    calouro2.membro.user = user
 
-      calouros_ordenados = helper.calouros_ordenados([calouro1, calouro2])
+    calouros_ordenados = helper.calouros_ordenados([calouro1, calouro2])
 
-      expect(calouros_ordenados).to eq([calouro2, calouro1])
-    end
+    expect(calouros_ordenados).to eq([calouro2, calouro1])
+  end
 end
