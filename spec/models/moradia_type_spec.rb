c95515669a1ef4bf01e90d6be6547e01e8c424e6
= <=>
2016-11-30 13:02:21 -0200

    Removed obsolete test (@vfaria, @gustavocovas)

--- spec/models/moradia_type_spec.rb
+++ spec/models/moradia_type_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe MoradiaType, type: :model do
-  let(:valid_attributes) do
-    FactoryGirl.attributes_for(:moradia_type)
-  end
-
-  before :all do
+  before :each do
+    valid_attributes = FactoryGirl.attributes_for(:moradia_type)
     @moradia_type = MoradiaType.create! valid_attributes
+
+    puts @moradia_type.inspect
   end
 
   it "created_at is not nil" do
@@ -18,9 +17,5 @@ RSpec.describe MoradiaType, type: :model do
     @moradia_type.updated_at = nil
     expect(@moradia_type).to_not be_valid
   end
-
-  it "description is nil" do
-    @moradia_type.description = nil
-    expect(@moradia_type).to be_valid
-  end
+ 
 end
