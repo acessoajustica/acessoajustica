724bcf6a6fe60ff75e55fd4722f36ac61d26c620
Gustavo Covas <gtcovas@gmail.com>
2016-12-02 12:44:04 -0200

    Remove obsolete debug printing (@gustavocovas, @rCamposCruz)

--- spec/models/moradia_type_spec.rb
+++ spec/models/moradia_type_spec.rb
@@ -4,18 +4,16 @@ RSpec.describe MoradiaType, type: :model do
   before :each do
     valid_attributes = FactoryGirl.attributes_for(:moradia_type)
     @moradia_type = MoradiaType.create! valid_attributes
-
-    puts @moradia_type.inspect
   end
 
   it "created_at is not nil" do
     @moradia_type.created_at = nil
     expect(@moradia_type).to_not be_valid
   end
- 
+
   it "updated_at is not nil" do
     @moradia_type.updated_at = nil
     expect(@moradia_type).to_not be_valid
   end
- 
+
 end
