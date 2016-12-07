17ec5a10aeacb789f624ca852894998a6bd3eb82
Rafael Campos Cruz <raposa@raposa.home>
2016-12-07 11:13:57 -0200

    Add test for invalid name. (@rCamposCruz, @ericghlee)

--- spec/models/pessoa_spec.rb
+++ spec/models/pessoa_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Pessoa, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    
+  before :all do
+    @pessoa = Pessoa.new(FactoryGirl.attributes_for(:pessoa))
+  end
+
+  it "has invalid name" do
+    @pessoa.nome = nil
+    expect(@pessoa).to_not be_valid
+  end
+
 end
